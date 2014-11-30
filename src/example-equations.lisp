d16adccda913f8788255c202ea8a8b86220c9b60
Rosnec <rosnec.dev@gmail.com>
2014-11-30 17:41:04 -0500

    Added another example

--- src/example-equations.lisp
+++ src/example-equations.lisp
@@ -6,3 +6,10 @@
                       0
                       1
                       50))
+
+(defconstant example-1st-order-ode-xsiny
+  (make-1st-order-ode (lambda (x y) (* x (sin y)))
+                      1
+                      0
+                      1
+                      10))
